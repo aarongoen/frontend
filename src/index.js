2181d8f7ccccec2f12329b36c18d09cc8ed30ad8
aarongoen <aarongoen2.0@gmail.com>
2021-03-09 13:46:07 -0500

    Creates renderPieces function.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const renderPieces = function (pieces) {
     console.log(pieces)
 
     pieces.forEach(piece => {
-        rootEl.innerHTML += `<h2>${piece.name} -${piece.composer}</h2>`
+        rootEl.innerHTML += `<h2>${piece.name} -${piece.composer}</h2>
+        <p>period: ${piece.period}</p>
+        <p>key: ${piece.key}</p>
+        <p>length: ${piece.length}</p>
+        <p>liked?: ${piece.like}</p>`
     })
 }
